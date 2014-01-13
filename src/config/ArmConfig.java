7a98d789262fca77080a062da7eaab03b3ebf6d0
Ankith Uppunda <auppunda@gmail.com>
2014-01-12 17:40:36 -0800

    finished arm module

--- src/config/ArmConfig.java
+++ src/config/ArmConfig.java
@@ -11,5 +11,7 @@ package config;
  * @author Ankith Uppunda
  */
 public class ArmConfig {
-    
+   public static int p = 0;
+   public static int i = 0;
+   public static int d = 0;
 }
--- src/modules/ArmModule.java
+++ src/modules/ArmModule.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.networktables2.util.Set;
 
 /**
  *
@@ -20,43 +21,47 @@ public class ArmModule extends Module implements PIDOutput {
     private PIDController controller;
     private Encoder e;
     private Victor roller1, roller2, arm1, arm2;
-    public ArmModule(int v1, int v2, int v3, int v4, int edport1, int edport2){
+    double s;
+    public ArmModule(int v1, int v2, int v3, int v4, int edport1, int edport2, double s){
        controller = new PIDController(ArmConfig.p, ArmConfig.i, ArmConfig.d,e,this);
        e = new Encoder(edport1, edport2);
        roller1 = new Victor(v1);
        roller2 = new Victor(v2);
        arm1 = new Victor(v3);
        arm2 = new Victor(v4);
+       this.s = s;
     }
-    public synchronized vvoid setpositionA()
+    public synchronized void setpositionA()
     {
-        
+        controller.setSetpoint(0);
     }
     public synchronized void setpositionB()
     {
-        
+        controller.setSetpoint(180);
     }
     public synchronized void setpositionC()
     {
-        
+       controller.setSetpoint(45);
     }
     public synchronized void setpositionD()
     {
-        
+        controller.setSetpoint(90);
     }
     public synchronized void anyposition()
     {
-        
+       
     }
     public void run(){
         while(true)
         {
             if(enabled)
             {
+                s = e.get();
                 controller.enable();
             }
             else
             {
+                e.stop();
                 controller.disable();
             }
         }
