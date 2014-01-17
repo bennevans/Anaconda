6200e40bd446c1d107a8a81fe2395c8fbb6e4a97
Michael Chin <mchin188@yahoo.com>
2014-01-16 18:06:11 -0800

    give me money

--- Testing/XboxTankDrive/src/edu/wpi/first/wpilibj/templates/TankDrive.java
+++ Testing/XboxTankDrive/src/edu/wpi/first/wpilibj/templates/TankDrive.java
@@ -6,9 +6,8 @@
 /*----------------------------------------------------------------------------*/
 
 package edu.wpi.first.wpilibj.templates;
-
-
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,17 +21,16 @@ public class TankDrive extends SimpleRobot {
      * This function is called once each time the robot enters autonomous mode.
      */
     
-    Joystick left, right;
-    Victor left1, left2, right1, right2;
     XBox xbox;
+    Victor left1, left2, right1, right2;
     Servo camera;
+    
     
     protected void robotInit() {
         
         System.out.println("robotInit()");
         
-        left = new Joystick(1);
-        right = new Joystick(2);
+        xbox = new Joystick(1);
         left1 = new Victor(1);
         left2 = new Victor(2);
         right1 = new Victor(3);
@@ -57,8 +55,8 @@ public class TankDrive extends SimpleRobot {
             
             Watchdog.getInstance().kill();
             
-            double leftpower = -left.getY();
-            double rightpower = right.getY();
+            double leftpower = -xbox.getlY();
+            double rightpower = xbox.getrY();
             
             leftpower = com.sun.squawk.util.MathUtils.pow(leftpower, 3);
             rightpower = com.sun.squawk.util.MathUtils.pow(rightpower, 3);
@@ -68,13 +66,13 @@ public class TankDrive extends SimpleRobot {
             right1.set(rightpower);
             right2.set(rightpower);
             
-            camera.setAngle((right.getZ()+1) * 90);
+            //camera.setAngle((.getZ()+1) * 90);
             if(counter % 50 == 0){
-                DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser1, 1, "LeftJoy: " + left.getY());
-                DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1, "RightJoy: " + right.getY());
+                DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser1, 1, "LeftJoy: " + xbox.getlY());
+                DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1, "RightJoy: " + xbox.getrY());
                 DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser3, 1, "LeftPower: " + leftpower);
                 DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser4, 1, "RightPower: " + rightpower);
-                DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "Servo: " + camera.getAngle());
+                //DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "Servo: " + camera.getAngle());
                 DriverStationLCD.getInstance().updateLCD();
             }
             
