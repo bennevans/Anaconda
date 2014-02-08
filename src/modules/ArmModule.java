518c6a9b85aef8e012ffb69cc2e4c777a24051b6
Ben Evans <otterdude97@gmail.com>
2014-02-08 15:22:57 -0800

    Updated ArmModule

--- src/modules/ArmModule.java
+++ src/modules/ArmModule.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 public class ArmModule extends Module implements PIDOutput {
     private PIDController controller;
     private Potentiometer pot;
-    private Victor roller1, roller2, arm1, arm2;
+    private Victor roller, arm;
     
     /**
      * constructor for ArmModule
@@ -28,55 +28,22 @@ public class ArmModule extends Module implements PIDOutput {
      * @param potPort potentiometer port
      */
     
-    public ArmModule(int v1, int v2, int v3, int v4, int potPort){
+    public ArmModule(int rollerPort, int armPort, int potPort){
        pot = new AnalogPotentiometer(potPort);
        controller = new PIDController(ArmConfig.p, ArmConfig.i, ArmConfig.d,pot,this);
-       roller1 = new Victor(v1);
-       roller2 = new Victor(v2);
-       arm1 = new Victor(v3);
-       arm2 = new Victor(v4);
+       roller = new Victor(rollerPort);
+       arm = new Victor(armPort);
     }
     /**
      * sets the roller on or off
      * @param on state of the roller
      */
     public synchronized void setRoller(boolean on){
-        if(on)
-        {
-            roller1.set(ArmConfig.ROLLER_SPEED);
-            roller2.set(-ArmConfig.ROLLER_SPEED);
+        if(on){
+            roller.set(ArmConfig.ROLLER_SPEED);
+        }else{
+            roller.set(0);
         }
-
-    }
-
-    /**
-     * sets the position of the arm to the left
-     */
-    public synchronized void setPosition_left()
-    {
-        setPosition(10);
-    }
-    /**
-     * sets the position of the arm to the angled
-     */
-    public synchronized void setPosition_right()
-    {
-        setPosition(85);
-    }
-    
-    /**
-     * sets the position of the arm to an angled position
-     */
-    public synchronized void setPosition_angled()
-    {
-       setPosition(45);
-    }
-    /**
-     * sets position of arm to slanted
-     */
-    public synchronized void setPosition_up()
-    {
-        setPosition(70);
     }
     
     /**
@@ -95,8 +62,7 @@ public class ArmModule extends Module implements PIDOutput {
      */
     public synchronized void setPosition(double setposition)
     {
-        //TODO calculate correct encoder tick numbers based on angle
-       double setPositionB = (200.0 * setposition)/360 ;
+       double setPositionB = setposition;
        controller.setSetpoint(setPositionB);
     }
     /**
@@ -112,6 +78,7 @@ public class ArmModule extends Module implements PIDOutput {
             else
             {
                 controller.disable();
+                controller.reset();
             }
         }
     }
@@ -126,15 +93,14 @@ public class ArmModule extends Module implements PIDOutput {
      * @param d pid output value
      */
     public void pidWrite(double d) {
-       arm1.set(d);
-       arm2.set(d);
+       arm.set(d);
     }
     /**
      * toString for ArmModule 
      * @return current field values as a string
      */
     public String toString(){
-        return "Roller Value:" + roller1.get() + "Pot Value:" + pot.get() + "PID error:" + controller.getError();
+        return "Roller Value: " + roller.get() + " Pot Value: " + pot.get() + " PID error: " + controller.getError();
     }
 
     /**
@@ -143,9 +109,9 @@ public class ArmModule extends Module implements PIDOutput {
      */
     
     public String getLogData(){
-        String line1 = "\t\t<data name=\"roller\" value=\""+roller1.get()+"\"/>\n";
+        String line1 = "\t\t<data name=\"roller\" value=\""+roller.get()+"\"/>\n";
         String line2 = "\t\t<data name=\"pot\" value=\""+pot.get()+"\"/>\n";
-        String line3 = "\t\t<data name=\"arm\" value=\""+arm1.get()+"\"/>";
+        String line3 = "\t\t<data name=\"arm\" value=\""+arm.get()+"\"/>";
         return line1 + line2 + line3;
     }
     
