73b80e8f6c604ce9aa392fb54827e534f27e5dbd
Ben Evans <otterdude97@gmail.com>
2014-02-16 11:30:45 -0800

    added pid controller for shooter

--- src/config/ShooterConfig.java
+++ src/config/ShooterConfig.java
@@ -7,11 +7,12 @@ package config;
  */
 public class ShooterConfig {
     
-    public final static int LIFTER = 2;
-    public final static int WINCH_SHIFTER = 3;
-    public final static int WINCH_VICTOR1 = 7;
-    public final static int WINCH_VICTOR2 = 8;
-    public final static int TOUCH_SENSOR = 6;
+    public static final int LIFTER = 2;
+    public static final int WINCH_SHIFTER = 3;
+    public static final int WINCH_VICTOR1 = 7;
+    public static final int WINCH_VICTOR2 = 8;
+    public static final int TOUCH_SENSOR = 6;
     
-    public final static double WINCH_SPEED = 1.0;
+    public static final double KP = 0, KI = 0, KD = 0;
+    public static final int LOADED_TICKS = 0;
 }
